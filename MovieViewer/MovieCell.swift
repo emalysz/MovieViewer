63bfc9fe56f5e9c3bde4d62a42fceac1922de1a4
Emma Malysz <emalysz@usc.edu>
2016-10-04 19:12:04 -0700

    first commit

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Emma Malysz on 10/3/16.
+//  Copyright © 2016 Emma Malysz. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
